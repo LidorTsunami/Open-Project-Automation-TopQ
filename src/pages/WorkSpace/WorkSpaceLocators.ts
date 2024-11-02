f619002b4b6cdff6844b3b358b8a8194d3af5ba0
lidor.sabag <Lidor280006@gmail.com>
2024-11-03 00:48:15 +0200

    refactored

--- src/pages/WorkSpace/WorkSpaceLocators.ts
+++ src/pages/WorkSpace/WorkSpaceLocators.ts
@@ -2,7 +2,7 @@ export class WorkSpaceLocators {
     public readonly filterButton = "#work-packages-filter-toggle-button"
     public readonly createDropdown: string = "button.-primary.add-work-package"
     public readonly taskButton: string = "a[role='menuitem']:has(span:text('Task'))"
-    public readonly taskOptionsDropdown: string = "#content-body > openproject-base > div > ui-view > work-packages-base > div > ui-view > wp-view-page > div > div.work-packages-partitioned-page--content-container > div.work-packages-partitioned-page--content-right > op-wp-split-view > div > div.work-packages--details-toolbar-container > wp-details-toolbar > div > button"
+    public readonly taskOptionsDropdown: string = "//*[@id='content-body']/openproject-base/div/ui-view/work-packages-base/div/ui-view/wp-view-page/div/div[3]/div[2]/op-wp-split-view/div/div[2]/wp-details-toolbar/div/button"
     public readonly deleteTaskButton: string = "body > div.op-context-menu--overlay > ng-component > div > ul > li:nth-child(5) > a"
     public readonly confirmDeleteButton: string = '#wp_destroy_modal > div.spot-action-bar > div > button.button.button_no-margin.-danger.spot-action-bar--action'
 }
--- src/tests/TestSanity.test.ts
+++ src/tests/TestSanity.test.ts
@@ -1,14 +1,14 @@
 import {expect} from "@playwright/test";
-import {test} from "../utils/fixtures"
+import {test} from "../utils/fixtures";
 
 test('Add new Task and Filter it', async ({pageHolder, homepage}) => {
     await pageHolder.homePage.openProjects();
     await pageHolder.homePage.selectProjectDemo();
-    await pageHolder.navigationPage.enterWorkSpaces()
+    await pageHolder.navigationPage.enterWorkSpaces();
     await pageHolder.workspacePage.enterTaskMaker();
     await pageHolder.workspacePage.taskMakerPage.addTask("Another Awesome Task");
-    await pageHolder.workspacePage.openFilterPage()
-    await pageHolder.workspacePage.filterPage.filterTask("Another Awesome Task")
+    await pageHolder.workspacePage.openFilterPage();
+    await pageHolder.workspacePage.filterPage.filterTask("Another Awesome Task");
     const taskText: string | null = await pageHolder.workspacePage.taskMakerPage.getTaskText();
     expect(taskText).toBe("Another Awesome Task");
 });
